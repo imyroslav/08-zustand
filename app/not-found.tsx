cbb84691fe12bb05effc4afc7135f640e161fbf7
imyroslav <imyroslav@ukr.net>
2025-07-16 21:46:36 +0700

    add-meta-page-nfp

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,24 @@
+import type { Metadata } from "next";
 import Link from 'next/link';
 import css from "./not-found.module.css";
+
+export const metadata: Metadata = {
+  title: "Not found page",
+  description: "The page you are looking for does not exist.",
+  openGraph: {
+    title: "Page Not Found",
+    description: "The page you are looking for does not exist.",
+    url: "https://www.edu.goit.global/uk/account/login",
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: "note",
+      }
+    ]
+  }  
+}  
 
 export default function NotFound() {
   return (

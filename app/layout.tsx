4f883ab95363ee5a7b3d915e19b72f7f98d4c81e
imyroslav <imyroslav@ukr.net>
2025-07-18 00:30:29 +0700

    add-fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,14 @@ import type { Metadata } from "next";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import TanStackProvider from "../components/TanStackProvider/TanStackProvider";
+import { Roboto } from "next/font/google";
 
+const roboto = Roboto({
+  subsets: ['latin'], 
+  weight: ['400', '700'],
+  variable: '--font-roboto', 
+  display: 'swap', 
+});
 
 export const metadata: Metadata = {
   title: "Note Hub",
@@ -31,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.variable}>
         <TanStackProvider>
           <Header />
           {children}
